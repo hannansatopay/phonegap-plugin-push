b313e40ce5c45d314ce5ea9006727896a27435e6
8bhsolutions <48874658+8bhsolutions@users.noreply.github.com>
2019-09-25 08:25:31 +1000

    getToken is depreciated

--- src/android/com/adobe/phonegap/push/PushPlugin.java
+++ src/android/com/adobe/phonegap/push/PushPlugin.java
@@ -19,6 +19,8 @@ import android.util.Log;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import com.google.android.gms.tasks.Tasks;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
@@ -199,9 +201,11 @@ public class PushPlugin extends CordovaPlugin implements PushConstants {
             Log.v(LOG_TAG, "execute: senderID=" + senderID);
 
             try {
-              token = FirebaseInstanceId.getInstance().getToken();
+              token = Tasks.await(FirebaseInstanceId.getInstance().getInstanceId()).getToken();
             } catch (IllegalStateException e) {
               Log.e(LOG_TAG, "Exception raised while getting Firebase token " + e.getMessage());
+            } catch(Exception e) {
+              Log.e(LOG_TAG, "Exception raised while executing task - getting Firebase token " + e.getMessage(), e);
             }
 
             if (token == null) {
