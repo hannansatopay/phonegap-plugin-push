e08915d489cf1873a5fd719a6b70948d0f26542e
Hannan Satopay <sathannan@hotmail.com>
2020-07-23 20:41:21 +0530

    ...

--- src/android/com/adobe/phonegap/push/custom/ITextSubstitute.java
+++
@@ -1,8 +0,0 @@
-package com.adobe.phonegap.push;
-
-import android.content.Context;
-import android.os.Bundle;
-
-public interface ITextSubstitute {
-    String parse(String text, Bundle extras, Context appContext);
-}
